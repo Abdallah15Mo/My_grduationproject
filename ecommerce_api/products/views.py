96f8f2c97597cdd764bd9b5ece1582fe8b553efc
Abdallah mostafa <3bdallahgoat@gmail.com>
2025-08-30 21:23:58 +0300

    we are going to make this work

---
+++ ecommerce_api/products/views.py
@@ -0,0 +1,21 @@
+
+from rest_framework import viewsets, filters
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Product, Category
+from .serializers import ProductSerializer, CategorySerializer
+
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all().order_by('-created_at')
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
+    filter_backends = [filters.SearchFilter, DjangoFilterBackend]
+    search_fields = ["name", "category__name"]
+    filterset_fields = ["category", "price", "stock_quantity"]
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    permission_classes = [IsAuthenticatedOrReadOnly]
